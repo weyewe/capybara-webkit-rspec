a64bb97e835ca5eb84e87c180783f4815e8cf86d
willy <w.yunnal@gmail.com>
2015-10-06 18:01:30 +0700

    added active record model into integration testing

--- spec/features/willy_spec.rb
+++ spec/features/willy_spec.rb
@@ -13,14 +13,18 @@ describe "run with webkit" do
   	expect( 5 + 5 ).to eq(10)
   end
 
-  
+
 
   context "as a user" do
  	it "adds a news release" do
-      user = create(:user)
+      # user = create(:user)
+      user = User.create(email: 'admin@example.com',
+  password: 'secret', password_confirmation: 'secret', admin: true)
       sign_in(user)
       visit root_path
       click_link "News"
+
+      @initial_news_release = NewsRelease.count
 
       expect(page).to_not have_content "BigCo switches to Rails"
       click_link "Add News Release"
@@ -30,6 +34,10 @@ describe "run with webkit" do
       fill_in "Body",
         with: "BigCo has released a new website built with open source."
       click_button "Create News release"
+
+      @final_news_release = NewsRelease.count
+
+      expect( @final_news_release - @initial_news_release).to eq(1)
 
       expect(current_path).to eq news_releases_path
       expect(page).to have_content "Successfully created news release."
